6dda3400c8f519b5b4ca864eb6bef4029f3c91de
Eric  Hagood <eric@Erics-MacBook-Pro.local>
2020-08-14 10:08:41 -0500

    Added more logic to the map which should allow it to display properly

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,13 +2,37 @@ import React, { Component } from 'react'
 import SimpleMap from './SimpleMap'
 
 export default class Map extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            latitude: 0,
+            longitude: 0,
+        }
+    }
+
+    generateCoordinates(){
+        let lng = (Math.random() * 360 - 180).toFixed(6)
+        let lat = (Math.random() * 180 - 90).toFixed(6)
+        let latlng = lat.toString() + ',' + lng.toString()
+        this.setState({
+            latitude: lat,
+            longitude: lng
+        })
+        // this.setState({latlng: latlng})
+        fetch("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + latlng + "&key=" + process.env.GOOGLE_MAPS_API_KEY).then(response =>{
+            return response.json()
+        }).then (data => {
+            console.log(data)
+        })
+    }
+
     render() {
         return (
             <div>
                 <form>
                     
                 </form>
-                <SimpleMap />
+                <SimpleMap lat={this.state.latitude} lng={this.state.longitude}/>
             </div>
             
         )
--- src/components/SimpleMap.jsx
+++ src/components/SimpleMap.jsx
@@ -4,12 +4,32 @@ import GoogleMapReact from 'google-map-react'
 const AnyReactComponent = ({text}) => <div>{text}</div>
 
 export default class SimpleMap extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            center: {
+                lat: 0,
+                lng: 0
+            },
+            zoom: 11
+        }
+    }
+
+    componentDidMount(){
+        this.setState({
+            center:{
+                lat: this.props.lat,
+                lng: this.props.lng
+            }
+        })
+    }
+
     render() {
         return (
             <div>
                 <GoogleMapReact
-                    defaultCenter={this.props.center}
-                    defaultZoom={this.props.zoom}
+                    defaultCenter={this.center}
+                    defaultZoom={this.zoom}
                 >
                     <AnyReactComponent 
                         lat = {this.props.lat}
